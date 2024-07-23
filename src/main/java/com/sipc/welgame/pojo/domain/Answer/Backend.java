e2c304d107b9593c14b538dd3ca0f0c4250ceb35
birliigant <birliigantJesus@outlook.com>
2024-07-23 15:03:54 +0800

    total query

--- src/main/java/com/sipc/welgame/pojo/domain/Answer/Backend.java
+++ src/main/java/com/sipc/welgame/pojo/domain/Answer/Backend.java
@@ -9,7 +9,7 @@ public class Backend {
     public static final String BACKEND_ANSWER_ONE = "SQL";
     public static final List<String> BACKEND_ANSWER_TWO = new ArrayList<>();
     public static final String BACKEND_ANSWER_THREE = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJCYWNrZW5kIjp7Imlubm92YXRpb24iOnRydWUsInBhc3Npb24iOnRydWUsImNvb3BlcmF0aW9uIjp0cnVlfX0.glu6zlKFvj_NQUQHkceMKtMrQiYy_IMdskP-egBREjM";
-    public static final String BACKEND_ANSWER_FOUR = "innovation passion cooperation Wellcome to SIPC115 !!!";
+    public static final String BACKEND_ANSWER_FOUR = "innovation passion cooperation Welcome to SIPC115 !!!";
     static {
         BACKEND_ANSWER_TWO.add("1升,3降,4降");
         BACKEND_ANSWER_TWO.add("1升,3降,2降");
